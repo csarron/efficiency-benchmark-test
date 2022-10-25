33962cd3507d921968065e046654a0e5e9bfb2b7
Arron Cao <qqcao.cs@gmail.com>
2022-10-25 11:11:55 -0700

    add test logs

---
+++ run.py
@@ -0,0 +1,8 @@
+import time
+import torch
+a = torch.randn(2560, 500, 500).cuda()
+for i in range(50000):
+    c = torch.randn(256, 500, 500)
+    d = torch.einsum("bmn,bnk->bmk", c, c)
+    b = torch.einsum("bmn,bnk->bmk", a, a)
+    time.sleep(0.0001)
